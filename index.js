2cf8baabd241519f329e30a8039ace801e76d3e4
Vitthal Saxena <visaxen@iu.edu>
2024-02-05 20:58:44 +0000

    Weather ETL v2: Log data

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const {Storage}=require('@google-cloud/storage');
+const csv=require('csv-parser');
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+//     console.log(`  Event: ${context.eventId}`);
+//     console.log(`  Event Type: ${context.eventType}`);
+//     console.log(`  Bucket: ${file.bucket}`);
+//     console.log(`  File: ${file.name}`);
+
+    const gcs=new Storage();
+    const dataFile=gcs.bucket(file.bucket).file(file.name);
+    dataFile.createReadStream()
+    .on('error',()=>{
+        //Error Handling
+        console.error(error);
+    })
+    .pipe(csv())
+    .on('data',(row)=>{
+        //Log data
+        console.log(row);
+    })
+    .on('end',()=>{
+        //EOF
+        console.log("End of File!");
+    })
 }
